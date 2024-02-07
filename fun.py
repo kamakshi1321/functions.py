f5a1d999d5a0226ec3f7e820bd4bda4e3ba28e60
kamakshi1321 <159157322+kamakshi1321@users.noreply.github.com>
2024-02-07 22:21:23 +0530

    Create fun.py

---
+++ fun.py
@@ -0,0 +1,8 @@
+#program to evaluate whether numnber is div by 5
+def divisiblity_test(number):
+  print("given list: ",number)
+  number=[10,20,33,46,55]
+  if(number%5==0):
+    return True
+  else:
+    return False
